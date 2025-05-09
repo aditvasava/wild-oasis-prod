31cbf32c69e53f2692716b7fbf9018f2af85f76d
Adit Vasava <asv2000p@gmail.com>
2025-05-09 12:35:47 -0400

    Updated generatestaticparams

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -9,15 +9,15 @@ export async function generateMetadata({ params }) {
   return { title: `Cabin ${name}` };
 }
 
-export async function generateStaticParams() {
-  const cabins = await getCabins();
+// export async function generateStaticParams() {
+//   const cabins = await getCabins();
 
-  const ids = cabins.map((cabin) => {
-    cabinId: String(cabin.id);
-  });
+//   const ids = cabins.map((cabin) => {
+//     cabinId: String(cabin.id);
+//   });
 
-  return ids;
-}
+//   return ids;
+// }
 
 export default async function Page({ params }) {
   const cabin = await getCabin(params.cabinId);
